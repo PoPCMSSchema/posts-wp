a8cca0ee3c0348079a96000d0deec6f183a1dcbc
Leo <losoviz@gmail.com>
2019-12-18 16:45:23 +0800

    Split `process` into `isInstanceOfType` and `isIDOfType`

--- src/TypeAPIs/PostTypeAPI.php
+++ src/TypeAPIs/PostTypeAPI.php
@@ -1,6 +1,7 @@
 <?php
 namespace PoP\PostsWP\TypeAPIs;
 
+use function get_post;
 use WP_Post;
 use PoP\Posts\TypeAPIs\PostTypeAPIInterface;
 /**
@@ -17,5 +18,31 @@ class PostTypeAPI implements PostTypeAPIInterface
     public function isInstanceOfPostType($object): bool
     {
         return ($object instanceof WP_Post) && $object->post_type == 'post';
+    }
+
+    /**
+     * Get the post with provided ID or, if it doesn't exist, null
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function getPost($id)
+    {
+        $post = get_post($id);
+        if (!$post || $post->post_type != 'post') {
+            return null;
+        }
+        return $post;
+    }
+
+    /**
+     * Indicate if an post with provided ID exists
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function postExists($id): bool
+    {
+        return $this->getPost($id) != null;
     }
 }
