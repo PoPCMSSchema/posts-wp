db0afbda4b28e9b183acaa0c24e10d1d416f827c
Leonardo Losoviz <leo@getpop.org>
2020-06-17 21:02:19 +0800

    Removed duplicate

--- src/TypeResolverPickers/Overrides/PostCustomPostTypeResolverPicker.php
+++ src/TypeResolverPickers/Overrides/PostCustomPostTypeResolverPicker.php
@@ -5,20 +5,12 @@ declare(strict_types=1);
 namespace PoP\PostsWP\TypeResolverPickers\Overrides;
 
 use PoP\Posts\Facades\PostTypeAPIFacade;
-use PoP\CustomPostsWP\TypeResolvers\Overrides\CustomPostUnionTypeResolver;
 use PoP\CustomPostsWP\TypeResolverPickers\CustomPostTypeResolverPickerInterface;
 use PoP\CustomPostsWP\TypeResolverPickers\NoCastCustomPostTypeResolverPickerTrait;
 
 class PostCustomPostTypeResolverPicker extends \PoP\Posts\TypeResolverPickers\Optional\PostCustomPostTypeResolverPicker implements CustomPostTypeResolverPickerInterface
 {
     use NoCastCustomPostTypeResolverPickerTrait;
-
-    public static function getClassesToAttachTo(): array
-    {
-        return [
-            CustomPostUnionTypeResolver::class,
-        ];
-    }
 
     public function getCustomPostType(): string
     {
